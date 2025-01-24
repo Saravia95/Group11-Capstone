c45b65dfff3b9eae919899599e9ea4f470263cb0
Hyunchul Cho <chc3954@gmail.com>
2025-01-23 21:30:54 -0500

    Setup Supabase and Prisma ORM

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import express, { Express, Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
+import { createClient } from '@supabase/supabase-js';
 
 //For env File
 dotenv.config();
+
+// Supabase Client
+const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!);
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
@@ -13,4 +17,11 @@ app.get('/', (req: Request, res: Response) => {
 
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
+  // For testing
+  supabase
+    .channel('room-1')
+    .on('postgres_changes', { event: '*', schema: 'public' }, (payload) => {
+      console.log('Change received!', payload);
+    })
+    .subscribe();
 });
