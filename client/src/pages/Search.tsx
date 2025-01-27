3d9f6de1a31bbbbfa2ed02c5961e4d00950c1d2d
Hyunchul Cho <chc3954@gmail.com>
2025-01-27 12:36:09 -0500

    Changed the search UI to include filter select

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -10,6 +10,11 @@ interface Song {
   playTime: string;
 }
 
+interface ISearchForm {
+  filter: string;
+  searchTerm: string;
+}
+
 const dummyList = Array.from({ length: 10 }, (_, i) => ({
   coverImage: 'coverImage',
   songTitle: `Song Title${i}`,
@@ -19,50 +24,70 @@ const dummyList = Array.from({ length: 10 }, (_, i) => ({
 
 const Search: React.FC = () => {
   const [searchResults, setSearchResults] = useState<Song[]>([]);
-  const { register, getValues, setValue, watch, handleSubmit } = useForm();
+  const { register, getValues, setValue, watch, handleSubmit } = useForm<ISearchForm>();
 
   const handleSearch = () => {
-    const { searchTerm } = getValues();
+    const { filter, searchTerm } = getValues();
+    console.log(filter, searchTerm);
 
-    const results = dummyList.filter(
-      (song) =>
-        song.songTitle.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        song.artistName.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        song.playTime.toLowerCase().includes(searchTerm.toLowerCase()),
-    );
+    let results: Song[] = [];
+
+    switch (filter) {
+      case 'title':
+        results = dummyList.filter((song) =>
+          song.songTitle.toLowerCase().includes(searchTerm.toLowerCase()),
+        );
+        break;
+      case 'artist':
+        results = dummyList.filter((song) =>
+          song.artistName.toLowerCase().includes(searchTerm.toLowerCase()),
+        );
+        break;
+    }
 
     setSearchResults(results);
-  };
-
-  const onClear = () => {
-    setValue('searchTerm', '');
   };
 
   return (
     <div className="container-sm flex flex-col">
       <h2 className="title">Search</h2>
-      <form className="form mt-10" onSubmit={handleSubmit(handleSearch)}>
-        <div className="relative flex items-center">
+      <form
+        className="w-full p-4 text-slate-300 focus:outline-none mt-10"
+        onSubmit={handleSubmit(handleSearch)}
+      >
+        <div className="relative flex">
+          <select
+            {...register('filter', { required: true })}
+            className="bg-slate-950 h-12 px-5 rounded-l-full text-sm focus:outline-none border-2 border-slate-300 border-r-1 cursor-pointer overflow-y-hidden"
+          >
+            <option className="font-medium cursor-pointer" value="title">
+              Title
+            </option>
+            <option className="font-medium cursor-pointer" value="artist">
+              Artist
+            </option>
+          </select>
           <input
             {...register('searchTerm', { required: true, min: 2 })}
             type="search"
-            className="w-full pl-12 py-2 rounded-full bg-slate-950 border-2 border-slate-300 font-light focus:outline-none focus:border-white transition-colors"
-            placeholder="Search songs"
+            placeholder="Search"
+            className="w-full bg-slate-950 h-12 flex px-5 rounded-r-full text-sm focus:outline-none border-2 border-l-0 border-slate-300"
           />
-          <span className="absolute left-4">
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-          </span>
-          {watch('searchTerm') && (
-            <span className="absolute right-4">
-              <button
-                type="button"
-                className="text-md text-slate-300 hover:text-slate-500 rounded-full cursor-pointer"
-                onClick={onClear}
-              >
-                <FontAwesomeIcon icon={faCircleXmark} />
-              </button>
-            </span>
-          )}
+          <button type="submit" className="absolute inset-y-0 right-0 mr-2 flex items-center px-2">
+            <svg
+              className="h-4 w-4 fill-current dark:text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              version="1.1"
+              id="Capa_1"
+              x="0px"
+              y="0px"
+              viewBox="0 0 56.966 56.966"
+              width="512px"
+              height="512px"
+            >
+              <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+            </svg>
+          </button>
         </div>
       </form>
       {/* Same UI with Playlist component */}
