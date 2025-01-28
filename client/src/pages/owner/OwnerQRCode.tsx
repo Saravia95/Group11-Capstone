d9ccef1565aa5ddbef14a617185a733dde2bed7e
Hyunchul Cho <chc3954@gmail.com>
2025-01-28 12:55:12 -0500

    Styled the qr code page

--- client/src/pages/owner/OwnerQRCode.tsx
+++ client/src/pages/owner/OwnerQRCode.tsx
@@ -1,29 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import QRCode from 'react-qr-code';
-import { useNavigate } from 'react-router';
+import { useAuthStore } from '../../stores/authStore';
 
 const OwnerQRCode: React.FC = () => {
-  const navigate = useNavigate();
-  const qrValue = 'https://google.com';
-  const navigateToOwnerSettings = () => {
-    navigate('/owner-settings');
-  };
+  const { user } = useAuthStore();
+  const [url, setUrl] = useState<string>('');
+
+  useEffect(() => {
+    setUrl(`http://localhost:3000/verify-qr/${user?.id}`);
+  }, [user]);
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-      }}
-    >
-      <button onClick={navigateToOwnerSettings}>Back</button>
-
-      <h1>Owner QR Code</h1>
-
-      <QRCode value={qrValue} size={256} />
+    <div className="container-sm">
+      <h2 className="title">Access QR Code</h2>
+      <div className="bg-white shadow mt-10 flex flex-col gap-10 items-center p-5 rounded-2xl">
+        <p className="text-center text-sm text-black">
+          Share this QR code with your customers to allow them to access the app
+        </p>
+        <QRCode value={url} size={256} />
+        <div className="flex justify-center gap-5">
+          <button className="button">Print</button>
+          <button className="button">Download</button>
+        </div>
+      </div>
     </div>
   );
 };
