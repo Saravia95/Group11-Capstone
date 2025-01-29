fe06ec4b3d3b773b800ce6dcdd10a7f899a51b80
Hyunchul Cho <chc3954@gmail.com>
2025-01-28 20:21:02 -0500

    Changed a minor fix again

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -2,9 +2,9 @@ import { Navigate, Outlet } from 'react-router';
 import { Role, useAuthStore } from '../stores/authStore';
 
 const ProtectedRoute = () => {
-  const { user } = useAuthStore();
+  const { isAuthenticated } = useAuthStore();
 
-  return user?.role === Role.Admin ? <Outlet /> : <Navigate to="/owner-login" replace />;
+  return isAuthenticated ? <Outlet /> : <Navigate to="/owner-login" replace />;
 };
 
 export default ProtectedRoute;
