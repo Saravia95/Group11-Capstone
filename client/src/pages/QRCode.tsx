3c2d67cf57c8bda65c1d6cc73a2408ebb916d6ca
Hyunchul Cho <chc3954@gmail.com>
2025-01-30 13:19:28 -0500

    Implemented QR code print with formatter feature

--- client/src/pages/QRCode.tsx
+++ client/src/pages/QRCode.tsx
@@ -13,34 +13,103 @@ const ManageQRCode: React.FC = () => {
 
   const handleDownload = () => {
     const svgElement = document.getElementById('qr-code') as HTMLCanvasElement;
-
     if (!svgElement) return;
 
+    // Convert the SVG element into a string
     const svgString = new XMLSerializer().serializeToString(svgElement);
-
+    // Create a Blob object from the SVG string
     const blob = new Blob([svgString], { type: 'image/svg+xml;charset=utf-8' });
+    // Generate a URL for the Blob
     const url = URL.createObjectURL(blob);
 
+    // Create a canvas element for rendering the SVG as an image
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
 
+    // Create an Image object to load the SVG Blob URL
     const img = new Image();
     img.onload = () => {
       canvas.width = img.width;
       canvas.height = img.height;
-
+      // Draw the image on the canvas
       ctx?.drawImage(img, 0, 0);
-
+      // Create a download link element
       const a = document.createElement('a');
+      // Set the href to the canvas data URL (as PNG)
       a.href = canvas.toDataURL('image/png');
-      a.download = 'downloaded-image.png';
+      // Set the filename for the downloaded image
+      a.download = `${user?.email}-qr-code.png`;
+      // Programmatically trigger the download
       a.click();
 
+      // Revoke the Blob URL to free up memory
       URL.revokeObjectURL(url);
     };
-
+    // Set the image source to the Blob URL
     img.src = url;
   };
+
+  const handlePrint = () => {
+    const svgElement = document.getElementById('qr-code') as HTMLCanvasElement;
+    if (!svgElement) return;
+
+    // Convert the SVG element into a string
+    const svgString = new XMLSerializer().serializeToString(svgElement);
+    // Open a new browser window or tab
+    const printWindow = window.open('', '_blank');
+
+    if (printWindow) {
+      // Open the document for writing
+      printWindow.document.open();
+      // Write the SVG string into the new document with proper HTML structure
+      printWindow.document.write(htmlFormatter(svgString));
+      // Close the document to render its contents
+      printWindow.document.close();
+
+      // Once the document has loaded, trigger the print dialog
+      printWindow.onload = () => {
+        printWindow.print();
+        // Close the print window after printing
+        printWindow.close();
+      };
+    }
+  };
+
+  const htmlFormatter = (svgString: string) => `
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Print QR Code</title>
+            <style>
+              body {
+                font-family: 'Arial', sans-serif;
+                margin: 0;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+              }
+              .box {
+                border: 3px solid #000;
+                padding: 10px 40px 40px 40px;
+                border-radius: 10px;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+              }
+            </style>
+          </head>
+          <body>
+            <h1>&#9835; JukeVibes</h1>
+            <div class="box">
+              <p>Scan this QR code to access the app</p>
+              <div>${svgString}</div>
+            <div>
+          </body>
+        </html>
+      `;
 
   return (
     <div className="container-sm">
@@ -51,7 +120,9 @@ const ManageQRCode: React.FC = () => {
         </p>
         <QRCode id="qr-code" value={url} size={256} />
         <div className="flex justify-center gap-5">
-          <button className="button">Print</button>
+          <button className="button" onClick={handlePrint}>
+            Print
+          </button>
           <button className="button" onClick={handleDownload}>
             Download
           </button>
