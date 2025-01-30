494ee63a47d1fca60eca91571db0692fc8a9b394
Gayoung <gykim8798@gmail.com>
2025-01-30 11:37:36 -0500

    Fix overlapping issue of search and clear (X) buttons, and add form reset after search

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -18,7 +18,7 @@ interface ISearchForm {
 const Search: React.FC = () => {
   const [searchResults, setSearchResults] = useState<Song[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const { register, getValues, handleSubmit } = useForm<ISearchForm>();
+  const { register, getValues, handleSubmit, reset } = useForm<ISearchForm>();
 
   const handleSearch = async () => {
     const { filter, searchTerm } = getValues();
@@ -27,6 +27,7 @@ const Search: React.FC = () => {
     try {
       const results = await searchSong(filter, searchTerm);
       setSearchResults(results);
+      reset({ filter, searchTerm: '' });
     } catch (error) {
       console.error('fail to search songs:', error);
     } finally {
@@ -53,35 +54,31 @@ const Search: React.FC = () => {
               Artist
             </option>
           </select>
-          <input
-            {...register('searchTerm', { required: true, min: 2 })}
-            type="search"
-            placeholder="Search"
-            className="w-full bg-slate-950 h-12 flex px-5 rounded-r-full text-sm focus:outline-none border-2 border-l-0 border-slate-300"
-          />
-          <button
-            type="submit"
-            disabled={isLoading}
-            className="absolute inset-y-0 right-0 mr-2 flex items-center px-2"
-          >
-            {isLoading ? (
-              <span>Searching...</span>
-            ) : (
-              <svg
-                className="h-4 w-4 fill-current dark:text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                version="1.1"
-                id="Capa_1"
-                x="0px"
-                y="0px"
-                viewBox="0 0 56.966 56.966"
-                width="512px"
-                height="512px"
-              >
-                <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-              </svg>
-            )}
-          </button>
+          <div className="relative flex-1">
+            <input
+              {...register('searchTerm', { required: true, min: 2 })}
+              type="search"
+              placeholder="Search"
+              className="w-full bg-slate-950 h-12 px-5 pr-12 rounded-r-full text-sm focus:outline-none border-2 border-l-0 border-slate-300"
+            />
+            <button
+              type="submit"
+              disabled={isLoading}
+              className="absolute right-0 inset-y-0 flex items-center px-4"
+            >
+              {isLoading ? (
+                <span>Searching...</span>
+              ) : (
+                <svg
+                  className="h-4 w-4 fill-current dark:text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 56.966 56.966"
+                >
+                  <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+                </svg>
+              )}
+            </button>
+          </div>
         </div>
       </form>
 
