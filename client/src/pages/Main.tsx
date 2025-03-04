1d8d041b492c4f873bfd123443b6da1c00b12091
Hyunchul Cho <chc3954@gmail.com>
2025-03-04 17:20:12 -0500

    Display 'Now Playing' on the main page on the customer side

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -3,15 +3,17 @@ import React from 'react';
 import NowPlaying from '../components/NowPlaying';
 import Playlist from '../components/Playlist';
 import { Helmet } from 'react-helmet-async';
+import { useAuthStore } from '../stores/authStore';
+import Player from '../components/Player';
+import { Role } from '../types/auth';
 
 const Main: React.FC = () => {
+  const { user } = useAuthStore();
+
   return (
     <div className="container w-full flex flex-col lg:flex-row h-[95vh]">
       <Helmet title="JukeVibes" />
-
-      {/* Now Playing */}
-      <NowPlaying />
-      {/* Toggle playist? */}
+      {user?.role === Role.ADMIN ? <Player /> : <NowPlaying />}
       <Playlist />
     </div>
   );
